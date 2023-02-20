32a299f6931936aa2f643b01f2c97d5a69f5c0e0
nusaybah97 <nusaybah.ar@outlook.com>
2023-02-20 16:14:35 +0000

    adds controller

--- controllers/games-controllers.js
+++ controllers/games-controllers.js
@@ -1,9 +1,19 @@
-const {fetchCategories} = require('../models/games-models')
+const {fetchCategories, fetchReviews} = require('../models/games-models')
 
 exports.getCategories = (req, res, next) => {
     return fetchCategories()
     .then((categories) => {
         res.status(200).send({categories});
+    })
+    .catch((err) => {
+        next(err);
+    })
+};
+
+exports.getReviews = (req,res,next) => {
+    return fetchReviews()
+    .then((reviews) => {
+        res.status(200).send({reviews})
     })
     .catch((err) => {
         next(err);
