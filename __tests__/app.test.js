160e83ecd84946ab89ddad6be2e7f5f0642a6c42
nusaybah97 <nusaybah.ar@outlook.com>
2023-02-20 16:12:32 +0000

    adds tests for get reviews

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,5 +1,6 @@
 const request = require('supertest');
 const {app} = require('../app');
+require('jest-sorted')
 
 const seed = require('../db/seeds/seed');
 const testData = require('../db/data/test-data/index');
@@ -30,5 +31,32 @@ describe('app', () => {
                 })
             })
         })
+    })
+        describe('/api/reviews', () => {
+            it('200 GET - responds with an array of review objects ordered by date', () => {
+                return request(app)
+                .get('/api/reviews').expect(200)
+                .then(({body}) => {
+                    expect(body.reviews).toBeInstanceOf(Array);
+                    expect(body.reviews).toHaveLength(13);
+                    body.reviews.forEach((review) => {
+                        expect(review).toMatchObject({
+                            owner: expect.any(String),
+                            title: expect.any(String),
+                            review_id: expect.any(Number),
+                            category: expect.any(String),
+                            review_img_url: expect.any(String),
+                            created_at: expect.any(String),
+                            votes: expect.any(Number),
+                            designer: expect.any(String),
+                            comment_count: expect.any(String),
+                        })
+                    })
+                    expect(body.reviews).toBeSortedBy('created_at', {
+                        descending: false
+                    })
+                })
+
+            })
+        })
     });
-});
