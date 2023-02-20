0c469415c6668937e2c3ec642dc5943f1bdd3dcf
nusaybah97 <nusaybah.ar@outlook.com>
2023-02-20 14:28:59 +0000

    made changes

--- app.js
+++ app.js
@@ -3,8 +3,6 @@ const {handle500Error} = require('./controllers/error-handling-controllers')
 const {getCategories} = require('./controllers/games-controllers')
 const app = express();
 
-app.use(express.json());
-
 app.get('/api/categories', getCategories);
 
 app.use(handle500Error)
