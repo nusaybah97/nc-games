158519085def7ef02383a54ef87db39ebb92891c
nusaybah97 <nusaybah.ar@outlook.com>
2023-02-20 16:14:02 +0000

    adds endpint for api reviews

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const {handle500Error} = require('./controllers/error-handling-controllers')
-const {getCategories} = require('./controllers/games-controllers')
+const {getCategories, getReviews} = require('./controllers/games-controllers')
 const app = express();
 
 app.get('/api/categories', getCategories);
+app.get('/api/reviews', getReviews);
 
 app.use(handle500Error)
+
 
 module.exports = {app};
