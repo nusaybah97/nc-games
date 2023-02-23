8e2f85457a0a108efc8dc8e90f259355579e3f94
nusaybah97 <nusaybah.ar@outlook.com>
2023-02-23 08:40:15 +0000

    adds error handling

--- controllers/error-handling-controllers.js
+++ controllers/error-handling-controllers.js
@@ -1,6 +1,14 @@
+exports.handleNonExistingPaths = (req, res, next) => {
+    res.status(404).send({message: 'Invalid Path!'})
+}
+
 exports.handlePSQL400Errors = (err, req, res, next) => {
     if (err.code === '22P02') {
         res.status(400).send({message: 'That is not a valid request!'});
+    } else if (err.code === '23502') {
+        res.status(400).send({message: 'That is not a valid request!'});
+    } else if (err.code === '23503') {
+        res.status(404).send({message: 'Sorry Not Found :('});
     } else {
         next(err);
     }
