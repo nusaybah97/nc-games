b1b9e4afbcfe2bf71ed954a09b742896889efb8d
nusaybah97 <nusaybah.ar@outlook.com>
2023-02-24 14:54:42 +0000

    hosting

--- db/connection.js
+++ db/connection.js
@@ -9,11 +9,11 @@ if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
   throw new Error('PGDATABASE or DATABASE_URL not set');
 }
 
-const config = (ENV = 'production'
+const config = (ENV === 'production'
 ? {
   connectionString: process.env.DATABASE_URL,
-  max2,
+  max: 2,
 }
 : {});
 
-module.exports = new Pool();
+module.exports = new Pool(config);
