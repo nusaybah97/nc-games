dcff1a7094f474902016a856be1bc3bd8dfd20f5
nusaybah97 <nusaybah.ar@outlook.com>
2023-03-13 10:48:40 +0000

    allow cross origin resource sharing

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const {handle500Error, handlePSQL400Errors, handleCustomErrors, handleNonExistingPaths} = require('./controllers/error-handling-controllers')
 const {getCategories, getReviews, getReviewById, getCommentsByReviewId, postCommentByReviewId, patchReviewById, getUsers, eraseCommentById, getApis} = require('./controllers/games-controllers')
 const app = express();
+const cors = require('cors');
 
 app.use(express.json())
 
@@ -9,17 +10,19 @@ app.get('/api', getApis);
 app.get('/api/categories', getCategories);
 app.get('/api/reviews', getReviews);
 app.get('/api/reviews/:id', getReviewById);
-app.get('/api/reviews/:review_id/comments', getCommentsByReviewId)
-app.post('/api/reviews/:review_id/comments', postCommentByReviewId)
-app.patch('/api/reviews/:review_id', patchReviewById)
-app.get('/api/users', getUsers)
-app.delete('/api/comments/:comment_id', eraseCommentById)
+app.get('/api/reviews/:review_id/comments', getCommentsByReviewId);
+app.post('/api/reviews/:review_id/comments', postCommentByReviewId);
+app.patch('/api/reviews/:review_id', patchReviewById);
+app.get('/api/users', getUsers);
+app.delete('/api/comments/:comment_id', eraseCommentById);
 
-app.use(handleNonExistingPaths)
+app.use(cors());
 
-app.use(handlePSQL400Errors)
-app.use(handleCustomErrors)
-app.use(handle500Error)
+app.use(handleNonExistingPaths);
+
+app.use(handlePSQL400Errors);
+app.use(handleCustomErrors);
+app.use(handle500Error);
 
 
 module.exports = {app};
